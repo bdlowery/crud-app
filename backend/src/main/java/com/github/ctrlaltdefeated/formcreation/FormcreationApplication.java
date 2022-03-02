4fb3f609b572101359c1efcd378f4311a47279da
Brian <bdlowery2@gmail.com>
2022-03-01 23:30:48 -0700

    initial commit

---
+++ backend/src/main/java/com/github/ctrlaltdefeated/formcreation/FormcreationApplication.java
@@ -0,0 +1,13 @@
+package com.github.ctrlaltdefeated.formcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormcreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormcreationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/ctrlaltdefeated/formcreation/FormcreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ctrlaltdefeated.formcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormcreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
